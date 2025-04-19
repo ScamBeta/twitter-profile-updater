c3b2aa5e1af1bf86e8789e0acc76d5aaad9deb2c
ScamBeta <scambait11@gmail.com>
2025-04-19 03:46:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def update_twitter():
 
     # Upload profile image
     try:
-        image_url = "https://i.imgur.com/WzzklgP.jpg"
+        image_url = "https://github.com/ScamBeta/twitter-profile-updater/blob/62f8ef37e895369a970f8433a00ec0eaa1cc9e2c/images/profile.jpg"
         image_path = "temp_profile.jpg"
 
         # Download image
